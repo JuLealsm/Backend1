a942656b01cef75148eadb33dc96d293c632f2b2
juliana <julianalealsm@hotmail.com>
2025-07-24 12:43:23 -0300

    entrega 2 basico

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { engine } from 'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import productsRouter from './src/routes/products.routes.js';
 import cartsRouter from './src/routes/carts.routes.js';
 
 const app = express();
-app.use(express.json());
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-// Rotas
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
+
+import { readData } from './src/utils/fileManager.js';
+app.get('/', (req, res) => {
+    const products = readData('products.json');
+    res.render('home', { products });
+});
+
+
+app.get('/realtimeproducts', (req, res) => {
+    const products = readData('products.json');
+    res.render('realTimeProducts', { products });
+});
+
+
+io.on('connection', socket => {
+    console.log('Cliente conectado!');
+
+    socket.on('addProduct', product => {
+        const products = readData('products.json');
+        const newProduct = { id: Date.now().toString(), ...product };
+        products.push(newProduct);
+        writeData('products.json', products);
+        io.emit('productsUpdated', products);
+    });
+
+    socket.on('deleteProduct', id => {
+        let products = readData('products.json');
+        products = products.filter(p => p.id !== id);
+        writeData('products.json', products);
+        io.emit('productsUpdated', products);
+    });
+});
+
 const PORT = 8080;
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
